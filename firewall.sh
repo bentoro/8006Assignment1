2937ad93503578e18808757caaee0f327d2ee61a
bentoro <bl_04@hotmail.com>
2018-01-25 13:21:03 -0800

    edited

--- firewall.sh
+++ firewall.sh
@@ -1,13 +1,27 @@
+$I = /sbin/iptables
+
 #save iptables
 /sbin/service iptables save
 
+
+
 #clear iptables
-iptables -F
+$I -F
 
 
 #drop all default policies
 iptables -P INPUT DROP
-iptables -P FORWARD DROP
 iptables -P OUTPUT DROP
 
-#
+#Drop SSH PORT 22
+iptables -A INPUT
+
+#Drop HTTP PORT 80
+
+#Drop HTTP
+
+#Drop all INBOUDN Syns
+
+#Allow DNS & DHCP at the TOP
+
+#443 should be open
