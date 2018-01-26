5536728735cb4c3628673983e53eb2252b915162
root <bl_04@hotmail.com>
2018-01-25 23:04:29 -0800

    up

--- firewall.sh
+++ firewall.sh
@@ -5,36 +5,34 @@ $IP -F
 
 #set default policies
 $IP -P INPUT DROP
+$IP -P FORWARD DROP
 $IP -P OUTPUT DROP
 
 #create user-defined chains
 $IP -N OUT
 $IP -N IN
 
-# Allow localhost
-$IP -A INPUT -p ALL -i lo -j ACCEPT
-
 #allow DHCP
-$IP -A INPUT -p UDP -s 0/0 --sport 67 --dport 68 -j ACCEPT
+$IP -A INPUT -p udp -m udp --sport 67 --dport 68 -j ACCEPT
 
 #allow from source ports 1024-65535
-$IP -A IN -p TCP -s 0/0 --dport 80 --sport 1024:65535 -j ACCEPT
+$IP -A INPUT -p tcp -m tcp --dport 80 --sport 1024:65535 -j ACCEPT
 
 #allow SSH
-$IP -A IN -p TCP -s 0/0 --dport 22 -j ACCEPT
+$IP -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
 
 #allow DHCP
-$IP -A OUTPUT -p UDP --dport 68 -m state --state NEW -j ACCEPT
+$IP -A OUTPUT -p udp -m udp --dport 68 -j ACCEPT
 
 #allow DNS
-$IP -A OUTPUT -p TCP --dport 53 -j ACCEPT
-$IP -A OUTPUT -p UDP --dport 53 -j ACCEPT
+$IP -A OUTPUT -p tcp --dport 53 -j ACCEPT
+$IP -A OUTPUT -p udp --dport 53 -j ACCEPT
 
 #allow HTTP
-$IP -A OUT -p TCP --sport 80 -j ACCEPT
-$IP -A OUT -p TCP --dport 80 -j ACCEPT
+$IP -A OUTPUT -p tcp -m tcp --sport 80 -j ACCEPT
+$IP -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
 
 #allow SSH
-$IP -A OUT -p TCP --sport 22 -j ACCEPT
-$IP -A OUT -p TCP --dport 22 -j ACCEPT
+$IP -A OUTPUT -p tcp -m tcp --sport 22 -j ACCEPT
+$IP -A OUTPUT -p tcp -m tcp --dport 22 -j ACCEPT
 
