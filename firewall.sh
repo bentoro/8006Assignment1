ff002cbc7b7aa7fab51cd650b56e2bede195fd31
root <bl_04@hotmail.com>
2018-01-25 23:11:53 -0800

    added sports

--- firewall.sh
+++ firewall.sh
@@ -13,16 +13,18 @@ $IP -N OUT
 $IP -N IN
 
 #allow DHCP
-$IP -A INPUT -p udp -m udp --sport 67 --dport 68 -j ACCEPT
-
+$IP -A INPUT -p udp -m udp --sport 67 -j ACCEPT
+$IP -A INPUT -p udp -m udp --dport 68 -j ACCEPT
 #allow from source ports 1024-65535
-$IP -A INPUT -p tcp -m tcp --dport 80 --sport 1024:65535 -j ACCEPT
-
+$IP -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+$IP -A INPUT -p tcp -m tcp --sport 1024:65535 -j ACCEPT
 #allow SSH
 $IP -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+$IP -A INPUT -p tcp -m tcp --sport 22 -j ACCEPT
 
 #allow DHCP
 $IP -A OUTPUT -p udp -m udp --dport 68 -j ACCEPT
+$IP -A OUTPUT -p udp -m udp --sport 68 -j ACCEPT
 
 #allow DNS
 $IP -A OUTPUT -p tcp --dport 53 -j ACCEPT
