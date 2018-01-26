a3e8e7303d6372fd0f0fd537065592f49cea4acc
root <bl_04@hotmail.com>
2018-01-25 23:42:30 -0800

    update

--- firewall.sh
+++ firewall.sh
@@ -3,6 +3,9 @@ IP="/sbin/iptables"
 #flush iptables
 $IP -F
 
+#Drop exisiting user-defined chains
+$IP -X
+
 #set default policies
 $IP -P INPUT DROP
 $IP -P FORWARD DROP
@@ -20,16 +23,17 @@ $IP -A INPUT -p udp -m udp --dport 0 -j DROP
 #allow DHCP
 $IP -A INPUT -p udp -m udp --sport 67 -j ACCEPT
 $IP -A INPUT -p udp -m udp --dport 68 -j ACCEPT
+
 #allow from source ports 1024-65535
-$IP -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
-$IP -A INPUT -p tcp -m tcp --sport 1024:65535 -j ACCEPT
+$IP -A INPUT -p tcp -m tcp --sport 80 --dport 1024:65535 -j ACCEPT
+
 #allow SSH
-$IP -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
 $IP -A INPUT -p tcp -m tcp --sport 22 -j ACCEPT
+$IP -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
 
 #allow DHCP
-$IP -A OUTPUT -p udp -m udp --dport 68 -j ACCEPT
 $IP -A OUTPUT -p udp -m udp --sport 68 -j ACCEPT
+$IP -A OUTPUT -p udp -m udp --dport 68 -j ACCEPT
 
 #allow DNS
 $IP -A OUTPUT -p tcp --dport 53 -j ACCEPT
