635be9a963529827f9f05b3f507b60a461acb7c2
bentoro <benedict.sh.lo@gmail.com>
2018-01-26 19:00:34 -0800

    working http

--- firewall.sh
+++ firewall.sh
@@ -15,35 +15,48 @@ $IP -P OUTPUT DROP
 $IP -N OUT
 $IP -N IN
 
+$IP -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
 $IP -A INPUT -p tcp -m tcp --sport 0 -j DROP
 $IP -A INPUT -p tcp -m tcp --dport 0 -j DROP
 $IP -A INPUT -p udp -m udp --sport 0 -j DROP
 $IP -A INPUT -p udp -m udp --dport 0 -j DROP
 
-#allow DHCP
-$IP -A INPUT -p udp -m udp --sport 67 -j ACCEPT
-$IP -A INPUT -p udp -m udp --dport 68 -j ACCEPT
 
-#allow from source ports 1024-65535
-$IP -A INPUT -p tcp -m tcp --sport 80 --dport 1024:65535 -j ACCEPT
-
-#allow SSH
-$IP -A INPUT -p tcp -m tcp --sport 22 -j ACCEPT
-$IP -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
-
-#allow DHCP
-$IP -A OUTPUT -p udp -m udp --sport 68 -j ACCEPT
-$IP -A OUTPUT -p udp -m udp --dport 68 -j ACCEPT
 
 #allow DNS
+$IP -A INPUT -p tcp --dport 53 -j ACCEPT
 $IP -A OUTPUT -p tcp --dport 53 -j ACCEPT
+$IP -A INPUT -p udp --dport 53 -j ACCEPT
 $IP -A OUTPUT -p udp --dport 53 -j ACCEPT
 
+#allow DHCP
+$IP -A OUTPUT -p udp --sport 67:68 --dport 67:68 -j ACCEPT
+$IP -A INPUT -p udp --sport 67:68 --dport 67:68 -j ACCEPT
+
+$IP -A OUTPUT -p tcp --dport 443 -j ACCEPT
+$IP -A INPUT -p tcp --dport 443 -j ACCEPT
+
+#allow HTTP
+$IP -A INPUT -p tcp -m tcp --dport 80 --sport 1024:65535 -j ACCEPT
 #allow HTTP
 $IP -A OUTPUT -p tcp -m tcp --sport 80 -j ACCEPT
 $IP -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
 
+#allow from source ports 1024-65535
+#$IP -A INPUT -p tcp -m tcp --dport 80 --sport 1024:65535 -j ACCEPT
+#$IP -A INPUT -p tcp -m tcp --sport 80 -j ACCEPT
+#$IP -A INPUT -p tcp -m tcp --dport 1024:65535 -j ACCEPT
+#allow HTTP
+#$IP -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
+
+#allow SSH
+$IP -A INPUT -p tcp -m tcp --sport 22 -j ACCEPT
+$IP -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
 #allow SSH
 $IP -A OUTPUT -p tcp -m tcp --sport 22 -j ACCEPT
 $IP -A OUTPUT -p tcp -m tcp --dport 22 -j ACCEPT
 
+#allow DNS
+#$IP -A OUTPUT -p tcp -m udp --sport 63 -j ACCEPT
+#$IP -A OUTPUT -p tcp -m udp --dport 63 -j ACCEPT
