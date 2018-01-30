f73bcb1bab94ca707adf7d7b8e779b9efe233821
bentoro <benedict.sh.lo@gmail.com>
2018-01-29 19:12:28 -0800

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,9 +1,9 @@
 IP="/sbin/iptables"
 
-TCPD=(0)
-TCPA=(53 443 80 22)
-UDPD=(0)
-UDPA=(53 67:68 80 22)
+TCPD='0'
+TCPA='53, 443, 80, 22'
+UDPD='0'
+UDPA='53, 67:68, 80, 22'
 
 #flush iptables
 $IP -F
@@ -34,39 +34,35 @@ $IP -A OUTPUT -p tcp -m tcp --dport 22 -j ACCT
 $IP -A OUTPUT -p tcp -m tcp --sport 80 -j ACCT
 $IP -A OUTPUT -p tcp -m tcp --dport 80 -j ACCT
 
-$IP -A INPUT -j TRAFFIC
-$IP -A INPUT -j TRAFFIC
+$IP -A INPUT -p all -j TRAFFIC
+$IP -A INPUT -p all -j TRAFFIC
 
-for i in "${TCPD}"
-do
-	:
+IFS=',' read -ra DROP <<< "$TCPD"
+for i in "${DROP[@]}"; do
 	$IP -A ACCT -p tcp -m tcp --sport $i -j DROP
 	$IP -A ACCT -p tcp -m tcp --dport $i -j DROP
 	$IP -A TRAFFIC -p tcp -m tcp --sport $i -j DROP
 	$IP -A TRAFFIC -p tcp -m tcp --dport $i -j DROP
 done
 
-for i in "${TCPA}"
-do
-	:
+IFS=',' read -ra ACCEPT <<< "$TCPA"
+for i in "${ACCEPT[@]}"; do
     $IP -A ACCT -p tcp -m tcp --sport $i -j ACCEPT
     $IP -A ACCT -p tcp -m tcp --dport $i -j ACCEPT
     $IP -A TRAFFIC -p tcp -m tcp --sport $i -j ACCEPT
     $IP -A TRAFFIC -p tcp -m tcp --dport $i -j ACCEPT
 done
 
-for i in "${UDPD}"
-do
-	:
-    $IP -A ACCT -p tcp -m tcp --sport $i -j DROP
-    $IP -A ACCT -p tcp -m tcp --dport $i -j DROP
-    $IP -A TRAFFIC -p tcp -m tcp --sport $i -j DROP
-    $IP -A TRAFFIC -p tcp -m tcp --dport $i -j DROP
+IFS=',' read -ra DROP <<< "$UDPD"
+for i in "${DROP[@]}"; do
+    $IP -A ACCT -p udp -m udp --sport $i -j DROP
+    $IP -A ACCT -p udp -m udp --dport $i -j DROP
+    $IP -A TRAFFIC -p udp -m udp --sport $i -j DROP
+    $IP -A TRAFFIC -p udp -m udp --dport $i -j DROP
 done
 
-for i in "${UDPA}"
-do
-	:
+IFS=',' read -ra ACCEPT <<< "$UDPA"
+for i in "${ACCEPT[@]}"; do
     $IP -A ACCT -p udp -m udp --sport $i -j ACCEPT
     $IP -A ACCT -p udp -m udp --dport $i -j ACCEPT
     $IP -A TRAFFIC -p udp -m udp --sport $i -j ACCEPT
